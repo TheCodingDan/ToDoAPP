b2b08b2bfa29f01c3d35ff3c893dd56e908d7777
TheCodingDan <dancgr2023@gmail.com>
2023-09-27 17:03:46 -0300

    NavBar Added

--- API/Program.cs
+++ API/Program.cs
@@ -35,6 +35,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
